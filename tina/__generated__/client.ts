ca5e456c4e4ae543f3cab380f55f9d0bd0dc80bb
Evan Mi <mixuanda@outlook.com>
2026-01-03 21:13:45 +0800

    Delete tina/__generated__ directory

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/mixuanda/Github/my_websites/tina/__generated__/.cache/1767438833760', url: 'http://localhost:4001/graphql', token: '', queries,  });
-export default client;
-  
