b1609994cfd453ba9942591005ea6d9fa6ab53a1
mixuanda <mixuanda@outlook.com>
2026-01-03 18:43:57 +0800

    feat: enable email-based account linking for GitHub and Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,7 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     })
   );
 }
@@ -20,6 +21,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     })
   );
 }
